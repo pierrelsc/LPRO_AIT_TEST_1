95876c9bdbd647298b559ffc9d39aaf2645994ad
pierrelsc <pierre.laissac@orange.fr>
2020-02-05 11:33:06 +0100

    Create test_button function

--- source/scripts/my_script.js
+++ source/scripts/my_script.js
@@ -1,3 +1,17 @@
 function init(){
     document.getElementById("test").innerHTML = "init says hi!";
 }
+
+
+/*
+*test_button
+*called by button onClick
+*/
+
+
+function test_button() {
+    var my_test_div = document.getElementById("test");
+    my_test_div.innerHTML = "button clicked!";
+    my_test_div.style.backgroundColor = "green";
+    alert("Cliked");
+}
